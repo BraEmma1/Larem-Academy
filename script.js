6e4f3db12d459a8a179d1c04317cca94366906d8
larem <emmanuelosei831@gmail.com>
2024-06-21 22:01:00 +0000

    ght

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+// function toggleNav() {
+//     let navLinks = document.getElementById("navLinks");
+//     let searchIcon = document.getElementById("searchIcon");
+//     if (navLinks.className === "nav-links") {
+//         navLinks.className += " responsive";
+//         searchIcon.style.display = "none";
+//     } else {
+//         navLinks.className = "nav-links";
+//         searchIcon.style.display = "block";
+//     }
+// }
+
+
+
+
+
 function toggleNav() {
-    let navLinks = document.getElementById("navLinks");
-    let searchIcon = document.getElementById("searchIcon");
-    if (navLinks.className === "nav-links") {
-        navLinks.className += " responsive";
-        searchIcon.style.display = "none";
-    } else {
-        navLinks.className = "nav-links";
-        searchIcon.style.display = "block";
-    }
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("nav-active");
 }
+
+// Add event listeners to menuIcon and closeIcon
+document.getElementById("menuIcon").addEventListener("click", function() {
+    toggleNav();
+});
+
+document.getElementById("closeIcon").addEventListener("click", function() {
+    toggleNav();
+});
